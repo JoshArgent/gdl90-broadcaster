672dbb000373b4050226afa5a7fce4760332da3b
Josh Argent <joshargent@mail.com>
2023-10-01 12:40:57 +0100

    Add JS docs

--- src/GDL90.js
+++ src/GDL90.js
@@ -2,23 +2,62 @@ import dgram from 'dgram';
 import { HearbeatMessage, Message } from './messages';
 import { Ownership } from './messages/ownership';
 
-const SOCKET = 4000;
-const HOST = '192.168.0.18';
+const DEFAULT_OPTIONS = {
+	dgram: dgram,
+	host: 'localhost',
+	port: 4000,
+};
+
 export class GDL90 {
 	/**
+	 * @private
 	 * @type {dgram}
 	 */
-	dgram;
+	_dgram;
 
+	/**
+	 * @private
+	 * @type {string}
+	 */
+	_host;
+
+	/**
+	 * @private
+	 * @type {number}
+	 */
+	_port;
+
+	/**
+	 * @private
+	 */
 	_socket;
 
-	constructor(dgram) {
-		this.dgram = dgram;
+	/**
+	 * @private
+	 */
+	_interval;
+
+	/**
+	 * @param {object} [options]
+	 * @param {dgram} [options.dgram] Datagram library for sending data over UDP. Defaults to Node's implementation
+	 * @param {string} [options.host] Host address to broadcast on. Defaults to localhost
+	 * @param {number} [options.port] Port to broadcast on. Defaults to 4000
+	 */
+	constructor(options = {}) {
+		const optionsWithDefaults = { ...DEFAULT_OPTIONS, ...options };
+
+		this._dgram = optionsWithDefaults.dgram;
+		this._host = optionsWithDefaults.host;
+		this._port = options.port;
 	}
 
+	/**
+	 * Opens the UDP connection and starts to GDL-90 broadcast
+	 * @returns {Promise} resolves once connection is established
+	 */
 	connect() {
 		return new Promise((resolve, reject) => {
-			this._socket = this.dgram.createSocket('udp4');
+			this._socket = this._dgram.createSocket('udp4');
 
 			this._socket.on('error', reject);
 
@@ -33,18 +72,37 @@ export class GDL90 {
 	}
 
 	/**
-	 * @param {Message} message
+	 * Closes the connection and stops the GDL-90 broadcast
 	 */
-	sendMessage(message) {
-		const messageBuffer = message.getValue();
+	close() {
+		if (this._interval) clearInterval(this._interval);
 
-		this._socket.send(messageBuffer, 0, messageBuffer.length, SOCKET, HOST);
+		this._socket.close();
 	}
 
+	/**
+	 * @private
+	 * @param {Message} message
+	 */
+	_sendMessage(message) {
+		const messageBuffer = message.getValue();
+
+		this._socket.send(
+			messageBuffer,
+			0,
+			messageBuffer.length,
+			this._port,
+			this._host
+		);
+	}
+
+	/**
+	 * @private
+	 */
 	_startHeartbeat() {
-		setInterval(() => {
-			this.sendMessage(new HearbeatMessage());
-			this.sendMessage(new Ownership());
+		this._interval = setInterval(() => {
+			this._sendMessage(new HearbeatMessage());
+			this._sendMessage(new Ownership());
 		}, 1000);
 	}
 }
